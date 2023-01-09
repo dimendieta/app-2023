ecf7db06b9ae30170b84ac6c3447294aceee1afc
dimendieta <91167870+dimendieta@users.noreply.github.com>
2023-01-09 14:48:31 -0500

    app-2023

---
+++ appmovil/src/app/models.ts
@@ -0,0 +1,36 @@
+
+export interface Cupon {
+     nombre: String;
+
+ }
+
+ export interface UserInfoI {
+    correo:string;
+    password:string;
+    nombre:string;
+    telefono:string;
+    uid:string;
+    perfil: 'visitante'|'admin'
+    
+    
+} 
+export interface ResponseApiSumaI{
+     respuesta: number;
+     estado:string;
+    }
+
+   export interface RequestApiSumaI{
+        nota1: number;
+        nota2: number;
+        nota3: number;
+      }
+
+      export interface ResultadoJuego{
+           id:string
+          intentos:number
+          tiempo:{
+               minutos:number
+               segundos:number
+          }
+          nivel:number
+      }
